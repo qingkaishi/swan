e9b7bcfb82fafa01bf44a70486a1e92ddc92d5b3
qingkaishi <qingkaishi@gmail.com>
2014-07-21 17:50:28 +0800

    axis: get related threads

--- axis/src/main/java/cn/edu/nju/software/axis/Axis.java
+++ axis/src/main/java/cn/edu/nju/software/axis/Axis.java
@@ -3,7 +3,6 @@
  * Developed by Qingkai Shi
  * Copy Right by the State Key Lab for Novel Software Tech., Nanjing University.  
  */
-
 package cn.edu.nju.software.axis;
 
 import cn.edu.nju.software.libevent.SwanEvent;
@@ -11,7 +10,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
 
 /**
@@ -80,6 +84,59 @@ public class Axis {
                 Vector<SwanEvent> trace = (Vector<SwanEvent>) ois.readObject();
                 ois.close();
 
+                List<SwanEvent> forks = new ArrayList<SwanEvent>();
+                Map<Integer, Vector<SwanEvent>> threads = new HashMap<Integer, Vector<SwanEvent>>();
+                for (SwanEvent se : trace) {
+                    if (!threads.containsKey(se.threadId)) {
+                        threads.put(se.threadId, new Vector<SwanEvent>());
+                    }
+
+                    if (se.accessType == SwanEvent.AccessType.FORK) {
+                        forks.add(se);
+                    }
+
+                    threads.get(se.threadId).add(se);
+                }
+
+                boolean find1 = false, find2 = false;
+                List<Integer> relatedThreads = new ArrayList<Integer>();
+                Iterator<Entry<Integer, Vector<SwanEvent>>> it = threads.entrySet().iterator();
+                while (it.hasNext()) {
+                    Entry<Integer, Vector<SwanEvent>> entry = it.next();
+                    int tid = entry.getKey();
+                    Vector<SwanEvent> track = entry.getValue();
+
+                    if (!find1 && containsAll(track, cl1, ln1)) {
+                        find1 = true;
+                        relatedThreads.add(tid);
+                    } else if (!find2 && containsAll(track, cl2, ln2)) {
+                        find2 = true;
+                        relatedThreads.add(tid);
+                    }
+
+                    if (find1 && find2) {
+                        break;
+                    }
+                }
+
+                if (!find1 || !find2) {
+                    throw new RuntimeException("Wrong input trace: cannot find the atomicity violations in the trace.");
+                }
+
+                int sz = -1;
+                int nsz = -1;
+                do {
+                    sz = relatedThreads.size();
+                    for (int i = 0; i < forks.size(); i++) {
+                        SwanEvent fk = forks.get(i);
+                        if (relatedThreads.contains(fk.sharedMemId)) {
+                            relatedThreads.add(fk.threadId);
+                        }
+                        forks.remove(i--);
+                    }
+                    nsz = relatedThreads.size();
+                } while (sz != nsz);
+
             } catch (Exception e) {
                 e.printStackTrace();
                 System.exit(1);
@@ -88,5 +145,29 @@ public class Axis {
         } else {
             throw new RuntimeException("Trace file error: " + f.getAbsolutePath() + ".");
         }
+    }
+
+    private static boolean containsAll(Vector<SwanEvent> aThread, List<String> clses, List<Integer> lns) {
+        boolean containsAll = true;
+
+        for (int i = 0; i < clses.size(); i++) {
+            String cls = clses.get(i);
+            int ln = lns.get(i);
+
+            boolean contains = false;
+            for (SwanEvent se : aThread) {
+                if (se.clsname.equals(cls) && se.lineNo == ln) {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (!contains) {
+                containsAll = false;
+                break;
+            }
+        }
+
+        return containsAll;
     }
 }
